336e81d02f3e755ef2fe5bc3d861422b62d87cee
jbreinercastro@gmail.com <jbreiner.castro@outlook.com>
2025-09-13 15:03:30 -0500

    proyecto base

---
+++ applications/app-service/src/main/java/co/com/crediya/reportes/config/UseCasesConfig.java
@@ -0,0 +1,14 @@
+package co.com.crediya.reportes.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration
+@ComponentScan(basePackages = "co.com.crediya.reportes.usecase",
+        includeFilters = {
+                @ComponentScan.Filter(type = FilterType.REGEX, pattern = "^.+UseCase$")
+        },
+        useDefaultFilters = false)
+public class UseCasesConfig {
+}
